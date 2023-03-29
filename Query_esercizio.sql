1f311bc30616ed0322cb721ba285fdb42425f489
Gabriele Pagnotta <gabriele.pagnotta1998@gmail.com>
2023-03-29 17:14:28 +0200

    GROUP BY

--- Query_esercizio.sql
+++ Query_esercizio.sql
@@ -60,3 +60,58 @@ SELECT videogame_id
 FROM reviews
 WHERE rating LIKE 5
 GROUP BY videogame_id
+
+
+/*GROUP BY */
+
+
+/*1- Contare quante software house ci sono per ogni paese (3)*/
+
+SELECT country
+FROM software_houses
+GROUP BY country
+
+/*2- Contare quante recensioni ha ricevuto ogni videogioco (del videogioco vogliamo solo l'ID) (500)*/
+
+SELECT videogame_id
+FROM reviews
+GROUP BY videogame_id
+
+/*3- Contare quanti videogiochi hanno ciascuna */
+/*classificazione PEGI (della classificazione PEGI vogliamo solo l'ID) (13)*/
+
+SELECT pegi_label_id
+FROM pegi_label_videogame
+GROUP BY pegi_label_id
+
+
+/*4- Mostrare il numero di videogiochi rilasciati ogni anno (11)*/
+
+SELECT YEAR(release_date) as release_date
+FROM videogames
+GROUP BY YEAR(release_date);
+
+/*5- Contare quanti videogiochi sono disponbili 
+per ciascun device (del device vogliamo solo l'ID) (7)*/
+
+SELECT COUNT(videogame_id) AS videogame_id
+FROM device_videogame
+GROUP BY device_id
+
+/*6- Ordinare i videogame in base alla media delle recensioni (del videogioco vogliamo solo l'ID) (500)*/
+
+SELECT AVG(rating)
+FROM reviews
+GROUP BY Videogame_id
+
+
+
+
+
+
+
+
+
+ 
+
+
